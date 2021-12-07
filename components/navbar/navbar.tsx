c081a0f180fe3108e9fb25cd9a6fbba7c31f7cba
gty <gefyoung@gmail.com>
2021-12-07 10:11:31 -0700

    navbar decent enoughed, fixed the whole nonclickable link thing, just style a instead, lol

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -11,28 +11,36 @@ const NavbarComp = ({ auth, notionId, username }) => {
   return (
     <div className="flex">
       <div className="flex-1"></div>
+      <div className="my-8 w-192">
+        <div className="flex flex-row">
 
-    <div className="m-8 ml-12 w-192">
-      <span className="px-2 py-1 mx-5 my-1 rounded hover:bg-gray-200 ">
-        <Link href="/">
-          <a onClick={() => aboutClicked()}>About</a>
-        </Link>
-      </span>
-      <span className="px-2 py-1 mx-5 my-1 rounded hover:bg-gray-200 ">
-        { notionId && <Link href={ '/' + username } >
-          <a>Your page</a>
-        </Link>}
-      </span>
-      <span className="px-3 py-2 mx-5 my-1 border-2 border-black border-solid rounded-2xl hover:bg-gray-200">
-        <Link href="/account">
-          <a>{auth ? "Account" : "Create your page"}</a>
-        </Link>
-      </span>
+          <Link href="/">
+            <a onClick={() => aboutClicked()} 
+              className="px-3 py-2 ml-5 mr-5 rounded-2xl hover:bg-gray-200">
+              About
+            </a>
+          </Link>
 
+          {notionId && <Link href={'/' + username} >
+            <a className="px-2 py-1 my-1 mr-5 rounded hover:bg-gray-200 ">
+              Your page
+            </a>
+          </Link>}
 
-    </div>
-    <div className="flex-1"></div>
-    </div>
+          <Link href="/account">
+            {auth 
+            ? <a className="px-3 py-2 rounded-2xl hover:bg-gray-200">
+              Account
+            </a>
+            : <a className="px-3 py-2 ml-10 border-2 border-black border-solid rounded-2xl hover:bg-gray-200">
+              Create your page
+            </a>}
+          </Link>
+
+        </div>
+      </div>
+      <div className="flex-1"></div>
+    </div >
   )
 }
 
